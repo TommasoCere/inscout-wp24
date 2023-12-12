0997c615a502fc19348c6f1faf077854cf3d595a
TommasoCere <cere.cere_2001@libero.it>
2023-12-12 11:50:10 +0100

    Add script to update web files

---
+++ script/updateWeb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Deve essere eseguito come superuser
+if [ "$EUID" -ne 0 ]; then
+    echo "Devi eseguire questo script come superuser."
+    exit
+fi
+
+# Nome della repository Git
+repository="inscout-wp24"
+
+# Directory locale della repository (sostituisci con il percorso della tua repository)
+repository_path="/home/tom/Progetto/inscout-wp24"
+
+# Cambia la directory alla repository locale
+cd "$repository_path"
+
+# Esegui il pull dalla repository remota
+git pull origin master
+
+# Se è richiesto il merge, eseguilo
+if [ -f "$repository_path/.git/MERGE_HEAD" ]; then
+    git commit -a -m "Merge"
+fi
+
+# Sposoto i file nella directory www
+cd /var/www/inscout.me
+rm -r *
+cp -r /home/tom/Progetto/inscout-wp24/www/* /var/www/inscout.me
+
+cd /var/www/app.inscout.me
+rm -r *
+cp -r /home/tom/Progetto/inscout-wp24/app/* /var/www/app.inscout.me
+
+echo "Pull completato per la repository $repository."
