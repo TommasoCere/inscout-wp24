174f6c828a8d28b57f6ce70a8abdad7cd8fb2642
Francesco Buda <francesco.buda3@studio.unibo.it>
2024-04-18 18:22:55 +0200

    refactoring e bug fixing

---
+++ controller/utility.js
@@ -0,0 +1,14 @@
+export function addHeaderFooter() {
+    // adds the header and footer to the page
+    fetch('http://localhost/app/base/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('header').innerHTML = data;
+        });
+
+    fetch('http://localhost/app/base/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('footer').innerHTML = data;
+        });
+}
