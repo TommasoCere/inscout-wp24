1aa4f814a9cbc32be6da0a54cd3393073d789673
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-05-13 16:13:21 +0200

    Add ExplorerResearch (Controllare se fa)

--- controller/ExplorePage.js
+++ controller/ExplorePage.js
@@ -7,17 +7,8 @@ async function getExploreAll() {
     return users;
 }
 
-async function getExplorerResearch() {
-    // DA PROVARE SE FUNZIONA !!!!!
-    // COME PASSIAMO IL NOME DA CERCARE ????
-    const response = await fetch("http://localhost/db/actions/getExplorerResearch.php", { method: "GET" });
-    const users = await response.json();
-    return users;
-}
-
 async function createExploreAllList() {
     // DA PROVARE SE FUNZIONA !!!!!
-    // DOVREBBERE ANDARE BENE PER TUTTI E DUE
     const feed = document.querySelector("#exploreAll");
     const users = await getExploreAll();
 
@@ -30,6 +21,58 @@ async function createExploreAllList() {
         clone.querySelector("#exploreAll p").innerHTML = user.username;
         feed.appendChild(clone);
     }
+}
+
+async function setExplorerResearch(stringa) {
+    try {
+        // Invia la richiesta POST al server
+        const response = await fetch("http://localhost/db/actions/getExplorerResearch.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "stringa=" + encodeURIComponent(stringa)
+        });
+
+        // Verifica se la risposta è stata ricevuta correttamente
+        if (!response.ok) {
+            throw new Error('Errore nella richiesta');
+        }
+
+        // Estrai e analizza la risposta JSON
+        const users = await response.json();
+
+        // Elabora la risposta
+        if (responseData.success) {
+            console.log(responseData.message);
+            // Puoi fare qualcosa con il messaggio di successo qui
+            return users;
+        } else {
+            console.error(responseData.message);
+            // Puoi fare qualcosa con il messaggio di errore qui
+        }
+    } catch (error) {
+        console.error('Si è verificato un errore:', error.message);
+        // Gestisci l'errore in modo adeguato
+    }
+}
+
+async function getExplorerResearch() {
+    // DA PROVARE SE FUNZIONA !!!!!
+    const feed = document.querySelector("#exploreAll");
+    stringa = document.querySelector("#ricerca").value;
+    const users = await setExplorerResearch(stringa);
+
+    const template = feed.querySelector("template");
+    for (let i=0; i<users.length; i++) {
+        let user = users[i];
+        let clone = template.content.cloneNode(true);
+        //CAMBIA POI CON FOTO DB
+        clone.querySelector("#exploreAll img").src = "/static/img/user.jpg";
+        clone.querySelector("#exploreAll p").innerHTML = user.username;
+        feed.appendChild(clone);
+    }
+    
 }
 
 addHeaderFooter();
--- db/actions/getExplorerResearch.php
+++ db/actions/getExplorerResearch.php
@@ -5,34 +5,43 @@ require_once("./../entities.php");
 use entities\User;
 
 global $driver;
-global $username; //VA BENE PER FARE RICERCA ??? BISOGNA PASSARE L'USERNAME DA CERCARE
 
-$sql = "SELECT UTENTI.* 
-FROM UTENTI
-WHERE UTENTI.username ="+$username;
-try {
-    $result = $driver->executeQuery($sql, $username);
-} catch (\Exception $e) {
-    throw new \Exception("Error while querying the database: " . $e->getMessage());
-}
-$users = array();
-if ($result->num_rows > 0) {
-    for ($i = 0; $i < $result->num_rows; $i++) {
-        $row = $result->fetch_array();
-        $user = new User(
-            $row['username'],
-            $row["profilePicturePath"],
-            $row["name"],
-            $row["surname"],
-            $row["email"],
-            $row["password"],
-            $row["section"],
-            $row["groupCity"],
-            $row["groupNumber"]
-        );
-        array_push($users, $user);
+// Controlla se è stata inviata una stringa
+if(isset($_POST['stringa'])) {
+    // Ottieni la stringa inviata
+    $stringa = $_POST['stringa'];
+    $sql = "SELECT UTENTI.* 
+        FROM UTENTI
+        WHERE UTENTI.username ="+$stringa;
+
+    try {
+        $result = $driver->executeQuery($sql, $username);
+    } catch (\Exception $e) {
+        throw new \Exception("Error while querying the database: " . $e->getMessage());
     }
+
+    $users = array();
+    if ($result->num_rows > 0) {
+        for ($i = 0; $i < $result->num_rows; $i++) {
+            $row = $result->fetch_array();
+            $user = new User(
+                $row['username'],
+                $row["profilePicturePath"],
+                $row["name"],
+                $row["surname"],
+                $row["email"],
+                $row["password"],
+                $row["section"],
+                $row["groupCity"],
+                $row["groupNumber"]
+            );
+            array_push($users, $user);
+        }
+    }
+    echo json_encode($users, JSON_PRETTY_PRINT);
+} else {
+    // Se nessuna stringa è stata inviata, restituisci un messaggio di errore
+    echo "Nessuna stringa ricevuta.";
 }
-echo json_encode($users, JSON_PRETTY_PRINT);
 
 ?>
