b562b38f375a04fa3e8aa95e5ae13d5b44a203eb
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-05-21 11:19:23 +0200

    Add explorer page (per ora non puoi cercare)

--- controller/ExplorePage.js
+++ controller/ExplorePage.js
@@ -1,78 +1,31 @@
-import { addHeaderFooter } from './utility.js';
+import { addHeaderFooter, getUserInfo, like, checkLike, isLogged, loadComments } from './utility.js';
+
 
 async function getExploreAll() {
-    // DA PROVARE SE FUNZIONA !!!!!
-    const response = await fetch("http://localhost/db/actions/user/getExploreAll.php", { method: "GET" });
-    const users = await response.json();
-    return users;
+    const response = await fetch("../../db/actions/user/getExploreAll.php", {
+        method: "GET"
+    });
+    const posts = await response.json();
+    return posts;
 }
 
-async function createExploreAllList() {
-    // DA PROVARE SE FUNZIONA !!!!!
+async function createFeed() {
     const feed = document.querySelector("#exploreAll");
     const users = await getExploreAll();
-
-    const template = feed.querySelector("template");
-    for (let i=0; i<users.length; i++) {
-        let user = users[i];
-        let clone = template.content.cloneNode(true);
-        //CAMBIA POI CON FOTO DB
-        clone.querySelector("#postHeader img").src = userInfo.fotoProfilo == null ? "/static/img/user.jpg" : userInfo.fotoProfilo;
-        clone.querySelector("#exploreAll p").innerHTML = user.username;
+  
+   const template = feed.querySelector("template");
+    for (let i = 0; i < users.length; i++) {
+      let user = users[i];
+      let clone = template.content.cloneNode(true);
+        clone.querySelector("#exploreAllImg img").src = user.profilePicturePath;
+        clone.querySelector("#exploreAllName p").innerHTML = user.username;
+        let likeBtn = clone.querySelector("#exploreAllName button");
         feed.appendChild(clone);
     }
 }
 
-async function setExplorerResearch(stringa) {
-    try {
-        // Invia la richiesta POST al server
-        const response = await fetch("http://localhost/db/actions/user/getExplorerResearch.php", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: "stringa=" + encodeURIComponent(stringa)
-        });
-
-        // Verifica se la risposta è stata ricevuta correttamente
-        if (!response.ok) {
-            throw new Error('Errore nella richiesta');
-        }
-
-        // Estrai e analizza la risposta JSON
-        const users = await response.json();
-
-        // Elabora la risposta
-        if (responseData.success) {
-            console.log(responseData.message);
-            // Puoi fare qualcosa con il messaggio di successo qui
-            return users;
-        } else {
-            console.error(responseData.message);
-            // Puoi fare qualcosa con il messaggio di errore qui
-        }
-    } catch (error) {
-        console.error('Si è verificato un errore:', error.message);
-        // Gestisci l'errore in modo adeguato
-    }
-}
-
-async function getExplorerResearch() {
-    // DA PROVARE SE FUNZIONA !!!!!
-    const feed = document.querySelector("#exploreAll");
-    stringa = document.querySelector("#ricerca").value;
-    const users = await setExplorerResearch(stringa);
-
-    const template = feed.querySelector("template");
-    for (let i=0; i<users.length; i++) {
-        let user = users[i];
-        let clone = template.content.cloneNode(true);
-        //CAMBIA POI CON FOTO DB
-        clone.querySelector("#postHeader img").src = userInfo.fotoProfilo == null ? "/static/img/user.jpg" : userInfo.fotoProfilo;
-        clone.querySelector("#exploreAll p").innerHTML = user.username;
-        feed.appendChild(clone);
-    }
-    
-}
+document.addEventListener('DOMContentLoaded', function() {
+    createFeed();
+});
 
 addHeaderFooter();
