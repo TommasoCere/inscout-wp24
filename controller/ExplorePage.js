e0eeb0dc050da87b48baa71a7e2d24ffafb3f99b
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2024-05-30 19:39:26 +0200

    TEST FIX EXPLORER

--- controller/ExplorePage.js
+++ controller/ExplorePage.js
@@ -12,11 +12,14 @@ async function getExploreAll() {
 async function getExplorerResearch() {
     const search = document.getElementById('ricerca').value;
     console.log(search);
-    const response = await fetch("../../db/actions/user/getExplorerResearch.php?stringaRicerca="+search, {
-        method: "GET"
-    });
-    const posts = await response.json();
-    return posts;
+    if(search != "" || search != null){
+        const response = await fetch("../../db/actions/user/getExplorerResearch.php?stringaRicerca="+search, {
+            method: "GET"
+        });
+        const posts = await response.json();
+        return posts;
+    }
+    return null;
 }
 
 async function createFeed() {
@@ -41,14 +44,18 @@ async function createFeedResearch() {
     const feed = document.getElementById("exploreAll");
     feed.innerHTML = "<template> <li class='mt-3 p-2 ps-4 mx-auto user-tag rounded-4' style='width: 9.6cm'> <div class='row align-items-center'> <div class='col-3' id='exploreAllImg'> <img src='/static/img/user.jpg' class='img-fluid rounded-circle' alt=''> </div> <div class='col-9' id='exploreAllName'> <p class='h4 text-start mb-2'> <a href='app/profile/profile.html'>cippa.lippa</a> </p> </div> </div>          </li> </template>";
     const users = await getExplorerResearch();
-    const template = feed.querySelector("template");
-    for (let i = 0; i < users.length; i++) {
-      let user = users[i];
-      let clone = template.content.cloneNode(true);
-        clone.querySelector("#exploreAllImg img").src = user.profilePicturePath;
-        clone.querySelector("#exploreAllName a").innerHTML =user.username;
-        clone.querySelector("#exploreAllName a").href="../../app/profile/profile.html?user="+user.username;
-        feed.appendChild(clone);
+    if(users == null){
+        createFeed();
+    }else{
+        const template = feed.querySelector("template");
+        for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        let clone = template.content.cloneNode(true);
+            clone.querySelector("#exploreAllImg img").src = user.profilePicturePath;
+            clone.querySelector("#exploreAllName a").innerHTML =user.username;
+            clone.querySelector("#exploreAllName a").href="../../app/profile/profile.html?user="+user.username;
+            feed.appendChild(clone);
+        }
     }
 }
 
@@ -61,7 +68,5 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     event.preventDefault();
     createFeedResearch();
 });
-
-document.getElementById("searchButton").addEventListener('click', getExplorerResearch());
 
 addHeaderFooter();
