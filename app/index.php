78bd39e8dbef1141075a14e3ae62470d2ec206b1
Tommaso <tommaso.ceredi@studio.unibo.it>
2024-06-07 17:01:05 +0200

    fix index.php

--- app/index.php
+++ app/index.php
@@ -2,13 +2,10 @@
 
 require_once(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
-if($username == null) {
+if ($username == null) {
     header("Location: ./auth/login/login.html");
     exit();
 } else {
     header("Location: ./home/home.html");
     exit();
 }
-
-
-?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+Header("Location: ./app/index.php");
